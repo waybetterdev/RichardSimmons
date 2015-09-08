58c19a2fa20e724dbb3f3b2e35562a3b377b6868
treindle <treindle@gmail.com>
2015-09-08 14:52:45 -0700

    Changing slack configuration

--- bin/starthubot.sh
+++ bin/starthubot.sh
@@ -1,4 +1,4 @@
-export HUBOT_SLACK_TOKEN=xoxb-6640409751-3qWhhpd5e5qwX1ZXd9YrMzh5 
+export HUBOT_SLACK_TOKEN=xoxb-10366194083-piUfKQUdROa7OSmkLAIg0npQ
 export HUBOT_JIRA_URL="https://waybetter.atlassian.net"
 export HUBOT_JIRA_USER="robot" 
 export HUBOT_JIRA_USER_AGENT="RichardSimmons"
