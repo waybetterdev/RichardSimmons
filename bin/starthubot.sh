a4771f589cc1b5c8d4026a42b945874ba68e5583
treindle <treindle@gmail.com>
2015-09-18 09:42:40 -0700

    INTER-56: Updating Jira config

--- bin/starthubot.sh
+++ bin/starthubot.sh
@@ -1,7 +1,7 @@
 export HUBOT_SLACK_TOKEN=xoxb-10366194083-piUfKQUdROa7OSmkLAIg0npQ
 export HUBOT_JIRA_URL="https://waybetter.atlassian.net"
 export HUBOT_JIRA_USER="robot" 
-export HUBOT_JIRA_USER_AGENT="richard_simmons"
+export HUBOT_JIRA_USER_AGENT="robot"
 export HUBOT_JIRA_PASSWORD="" #Add robot Password
 echo "Starting richard_simmons..."
 bin/hubot -a slack
