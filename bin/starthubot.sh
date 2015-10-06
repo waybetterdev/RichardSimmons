d0b80e91f16adad60547281090450a25a6dd5e4a
treindle <treindle@gmail.com>
2015-10-06 15:38:56 -0700

    Delete starthubot.sh

--- bin/starthubot.sh
+++
@@ -1,8 +0,0 @@
-export HUBOT_SLACK_TOKEN=xoxb-10366194083-piUfKQUdROa7OSmkLAIg0npQ
-export HUBOT_JIRA_URL="https://waybetter.atlassian.net"
-export HUBOT_JIRA_USER="robot" 
-export HUBOT_JIRA_USER_AGENT="RichardSimmons"
-export HUBOT_JIRA_PASSWORD="" #Add robot Password
-echo "Starting richard_simmons..."
-bin/hubot -a slack
-echo "richard_simmons has started."
